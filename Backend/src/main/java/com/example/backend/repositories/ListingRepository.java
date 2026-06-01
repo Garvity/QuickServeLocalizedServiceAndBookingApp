e8f374a7868003960a969ec6a0a97ad9ff345037
Garv  Changrani <154041471+Garvity@users.noreply.github.com>
2026-06-01 20:26:36 +0530

    Add method to find listings by unapproved status

--- Backend/src/main/java/com/example/backend/repositories/ListingRepository.java
+++ Backend/src/main/java/com/example/backend/repositories/ListingRepository.java
@@ -31,5 +31,6 @@ public interface ListingRepository extends JpaRepository<Listing, Long> {
     );
     List<Listing> findByProviderId(Long providerId);
     List<Listing> findByIsApprovedTrue();
+    List<Listing> findByIsApprovedFalse();
     List<Listing> findByIsApprovedNull();
 }
